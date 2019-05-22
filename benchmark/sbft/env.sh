ada69056958eb2e12f868f99dbdb1db999595baa
Your Name <you@example.com>
2019-05-22 22:09:48 +0800

    set logging level to warn

--- benchmark/sbft/env.sh
+++ benchmark/sbft/env.sh
@@ -2,5 +2,5 @@ SBFT_HOME=~/Migration/blockchain-perf/benchmark/sbft
 HOSTS=$SBFT_HOME/hosts
 CLIENTS=$SBFT_HOME/clients
 SBFT_DATA=$SBFT_HOME/data
-SBFT_SOURCE=$SBFT_DATA/data/concord-bft
+SBFT_SOURCE=$SBFT_DATA/concord-bft
 LOG_DIR=$SBFT_HOME/logs
