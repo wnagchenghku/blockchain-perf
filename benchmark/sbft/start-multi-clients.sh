d06c05c6e9a1c71fa9f5e41c16e7b2e9c6ab1524
Your Name <you@example.com>
2019-05-23 12:36:41 +0800

    begin to co-locate

--- benchmark/sbft/start-multi-clients.sh
+++ benchmark/sbft/start-multi-clients.sh
@@ -5,18 +5,23 @@ cd `dirname ${BASH_SOURCE-$0}`
 echo "start-multi-clients.sh"
 
 let i=0
+finished=false
 while :; do
   for client in `cat $CLIENTS`; do
     let client_id=$1+$i
     ssh -oStrictHostKeyChecking=no hkucs@$client $SBFT_HOME/start-clients.sh $1 $2 $client_id $3 $4
     let i=$i+1
+    if [[ "$i" -lt "$2" ]]; then
+      continue
+    else
+      finished=true
+      break
+    fi
   done
 
-  if [[ "$i" -lt "$2" ]]; then
-  	continue
+  if [[ "$finished" = true ]]; then
+    break
   fi
-
-  break
 done
 
 let M=$1*10+240
