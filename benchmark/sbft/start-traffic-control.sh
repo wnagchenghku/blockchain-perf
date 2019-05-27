67b5aa6666d9125450261776efe6e35d11b207c3
Your Name <you@example.com>
2019-05-27 13:56:12 +0800

    more iterations

--- benchmark/sbft/start-traffic-control.sh
+++ benchmark/sbft/start-traffic-control.sh
@@ -9,8 +9,8 @@ for host in `cat $HOSTS`; do
   
   if [[ "$host" == "202.45.128.160" ]] || [[ "$host" = "202.45.128.161" ]]; then
   	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp5s0 root netem delay 100ms
-  elif [[ "$host" == "202.45.128.170" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp179s0d1 root netem delay 100ms
+  elif [[ "$host" == "202.45.128.170" ]] || [[ "$host" == "202.45.128.169" ]]; then
+  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp179s0 root netem delay 100ms
   elif [[ "$host" == "202.45.128.176" ]] || [[ "$host" == "202.45.128.177" ]] || [[ "$host" == "202.45.128.178" ]] || [[ "$host" == "202.45.128.179" ]] || [[ "$host" == "202.45.128.180" ]]; then
   	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp1s0f1 root netem delay 100ms
   elif [[ "$host" == "202.45.128.171" ]] || [[ "$host" == "202.45.128.172" ]] || [[ "$host" == "202.45.128.173" ]] || [[ "$host" == "202.45.128.174" ]] || [[ "$host" == "202.45.128.175" ]]; then
