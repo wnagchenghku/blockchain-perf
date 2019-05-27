77ca67935cde8617c9dc9a048a074217451776e3
Your Name <you@example.com>
2019-05-27 14:19:08 +0800

    more hosts

--- benchmark/sbft/stop-traffic-control.sh
+++ benchmark/sbft/stop-traffic-control.sh
@@ -9,7 +9,7 @@ for host in `cat $HOSTS`; do
   
   if [[ "$host" == "202.45.128.160" ]] || [[ "$host" = "202.45.128.161" ]]; then
   	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev enp5s0 root 
-  elif [[ "$host" == "202.45.128.170" ]] || [[ "$host" == "202.45.128.169" ]]; then
+  elif [[ "$host" == "202.45.128.170" ]]; then
   	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev enp179s0 root
   elif [[ "$host" == "202.45.128.176" ]] || [[ "$host" == "202.45.128.177" ]] || [[ "$host" == "202.45.128.178" ]] || [[ "$host" == "202.45.128.179" ]] || [[ "$host" == "202.45.128.180" ]]; then
   	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev enp1s0f1 root
