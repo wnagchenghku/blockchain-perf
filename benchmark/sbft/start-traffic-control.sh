d8e9b372280c48dafb471a06898d77d47bd91b06
Your Name <you@example.com>
2019-06-07 15:58:49 +0800

    random RTT

--- benchmark/sbft/start-traffic-control.sh
+++ benchmark/sbft/start-traffic-control.sh
@@ -5,18 +5,5 @@ cd `dirname ${BASH_SOURCE-$0}`
 
 i=0
 for host in `cat $HOSTS`; do
-  ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev lo root handle 1:0 netem delay 100msec
-  
-  if [[ "$host" == "202.45.128.160" ]] || [[ "$host" = "202.45.128.161" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp5s0 root netem delay 100ms
-  elif [[ "$host" == "202.45.128.170" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp179s0 root netem delay 100ms
-  elif [[ "$host" == "202.45.128.176" ]] || [[ "$host" == "202.45.128.177" ]] || [[ "$host" == "202.45.128.178" ]] || [[ "$host" == "202.45.128.179" ]] || [[ "$host" == "202.45.128.180" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp1s0f1 root netem delay 100ms
-  elif [[ "$host" == "202.45.128.171" ]] || [[ "$host" == "202.45.128.172" ]] || [[ "$host" == "202.45.128.173" ]] || [[ "$host" == "202.45.128.174" ]] || [[ "$host" == "202.45.128.175" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp1s0d1 root netem delay 100ms
-  elif [[ "$host" == "202.45.128.162" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc add dev enp5s0d1 root netem delay 100ms
-  fi
-
+  ssh -oStrictHostKeyChecking=no hkucs@202.45.128.171 'device=$(ip -oneline -4 addr show scope global | grep "10.22" | tr -s " " | tr "/" " " | cut -f 2 -d " "); sudo tc qdisc add dev $device root netem delay 100ms; sudo tc qdisc add dev lo root handle 1:0 netem delay 100msec'
 done
--- benchmark/sbft/stop-traffic-control.sh
+++ benchmark/sbft/stop-traffic-control.sh
@@ -5,18 +5,5 @@ cd `dirname ${BASH_SOURCE-$0}`
 
 i=0
 for host in `cat $HOSTS`; do
-  ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev lo root
-  
-  if [[ "$host" == "202.45.128.160" ]] || [[ "$host" = "202.45.128.161" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev enp5s0 root 
-  elif [[ "$host" == "202.45.128.170" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev enp179s0 root
-  elif [[ "$host" == "202.45.128.176" ]] || [[ "$host" == "202.45.128.177" ]] || [[ "$host" == "202.45.128.178" ]] || [[ "$host" == "202.45.128.179" ]] || [[ "$host" == "202.45.128.180" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev enp1s0f1 root
-  elif [[ "$host" == "202.45.128.171" ]] || [[ "$host" == "202.45.128.172" ]] || [[ "$host" == "202.45.128.173" ]] || [[ "$host" == "202.45.128.174" ]] || [[ "$host" == "202.45.128.175" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev enp1s0d1 root 
-  elif [[ "$host" == "202.45.128.162" ]]; then
-  	ssh -oStrictHostKeyChecking=no hkucs@$host sudo tc qdisc del dev enp5s0d1 root
-  fi
-
+  ssh -oStrictHostKeyChecking=no hkucs@202.45.128.171 'device=$(ip -oneline -4 addr show scope global | grep "10.22" | tr -s " " | tr "/" " " | cut -f 2 -d " "); sudo tc qdisc del dev $device root; sudo tc qdisc del dev lo root'
 done
